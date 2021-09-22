3966d7355375a8cc52710f8c1355fd15e0fc3d32
Huan LI (李卓桓) <zixia@zixia.net>
2021-09-23 02:55:51 +0800

    clean

--- tests/fixtures/smoke-testing.ts
+++ tests/fixtures/smoke-testing.ts
@@ -6,5 +6,5 @@ import {
 }           from 'tstest'
 
 test('VERSION', async t => {
-  t.notEqual(VERSION, '0.0.0', 'should set version to the actual number')
+  t.not(VERSION, '0.0.0', 'should set version to the actual number')
 })
