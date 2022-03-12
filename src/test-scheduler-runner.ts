eabda8a42722362142a8d92669185f57571d0026
Huan (李卓桓) <zixia@zixia.net>
2022-03-12 15:37:25 +0800

    better message

--- src/test-scheduler-runner.ts
+++ src/test-scheduler-runner.ts
@@ -7,12 +7,12 @@ import type { TestClass } from './tap.js'
 
 const testSchedulerRunner = (callback: (helpers: RunHelpers) => void) => async (t: TestClass) => {
   const testScheduler = new TestScheduler((actual, expected) => {
-    t.equal(actual.length, expected.length, 'should have same number of events')
+    t.equal(actual.length, expected.length, 'should confirm the actual number of events be same as the expected')
 
     /**
      * Huan(202203): the test must contains at least one events
      */
-    t.not(actual.length, 'should has at least one marble events for testing')
+    t.not(actual.length, 'should has at least one marble event in the stream for testing')
 
     for (let i = 0; i < actual.length; i++) {
       t.same(actual[i], expected[i], `the marbals of actual is ${actual[i].frame}/${actual[i].notification.kind}:${JSON.stringify(actual[i].notification.value)}`)
