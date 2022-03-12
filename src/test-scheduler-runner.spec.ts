3d7a4d542a43c462e43729b348e74aae040500a1
Huan (李卓桓) <zixia@zixia.net>
2022-03-12 15:28:46 +0800

    more strict for RxJS testSchedulerRunner

--- src/test-scheduler-runner.spec.ts
+++ src/test-scheduler-runner.spec.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S node --no-warnings --loader ts-node/esm
 import { throttleTime } from 'rxjs'
 
-import { test }             from './tap.js'
-import { testSchedulerRunner } from './test-scheduler-runner.js'
+import { test }                 from './tap.js'
+import { testSchedulerRunner }  from './test-scheduler-runner.js'
 
 test('RxJS testSchedulerRunner: throttleTime demo testing', testSchedulerRunner(helpers => {
   const time = 200
@@ -14,3 +14,25 @@ test('RxJS testSchedulerRunner: throttleTime demo testing', testSchedulerRunner(
     helpers.cold(actual).pipe(operation),
   ).toBe(expected)
 }))
+
+test('RxJS testSchedulerRunner: fail with event number mismatch', async t => {
+  await t.rejects(testSchedulerRunner(helpers => {
+    const actual   = '-'
+    const expected = '-x'
+
+    helpers.expectObservable(
+      helpers.hot(actual),
+    ).toBe(expected)
+  }), 'should reject when the actual & expected event number is not match')
+})
+
+test('RxJS testSchedulerRunner: fail with 0 events', async t => {
+  await t.rejects(testSchedulerRunner(helpers => {
+    const actual   = '-'
+    const expected = '-'
+
+    helpers.expectObservable(
+      helpers.hot(actual),
+    ).toBe(expected)
+  }), 'should reject when there is no events in the stream')
+})
--- src/test-scheduler-runner.ts
+++ src/test-scheduler-runner.ts
@@ -7,6 +7,13 @@ import type { TestClass } from './tap.js'
 
 const testSchedulerRunner = (callback: (helpers: RunHelpers) => void) => async (t: TestClass) => {
   const testScheduler = new TestScheduler((actual, expected) => {
+    t.equal(actual.length, expected.length, 'should have same number of events')
+
+    /**
+     * Huan(202203): the test must contains at least one events
+     */
+    t.not(actual.length, 'should has at least one marble events for testing')
+
     for (let i = 0; i < actual.length; i++) {
       t.same(actual[i], expected[i], `the marbals of actual is ${actual[i].frame}/${actual[i].notification.kind}:${JSON.stringify(actual[i].notification.value)}`)
     }
